0f6a72c9c0f033a14f52bdf1624af4bf8ad34db5
Josh Jarmain <jjarmain@gmail.com>
2018-08-30 21:26:08 -0700

    wip on latest rn

--- packages/native/AppEntry.js
+++ packages/native/AppEntry.js
@@ -1,8 +1,21 @@
-import { KeepAwake, registerRootComponent } from "expo";
-import App from "./App";
+// import { KeepAwake, registerRootComponent } from "expo";
+// import App from "./App";
 
-if (__DEV__) {
-  KeepAwake.activate();
-}
+// if (__DEV__) {
+//   KeepAwake.activate();
+// }
 
-registerRootComponent(App);
+// registerRootComponent(App);
+
+
+import App from './App';
+import Expo from 'expo';
+import React from 'react';
+
+const AwakeInDevApp = props => [
+  <App key="app" {...props} />,
+  process.env.NODE_ENV === 'development' ? (
+    <Expo.KeepAwake key="keep-awake" />
+  ) : null,
+];
+Expo.registerRootComponent(AwakeInDevApp);
