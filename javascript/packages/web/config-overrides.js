fa56238ee41c25a3ca12db849065e8e5f187e936
Josh Jarmain <jjarmain@gmail.com>
2018-09-11 20:53:03 -0700

    clean up

--- javascript/packages/web/config-overrides.js
+++ javascript/packages/web/config-overrides.js
@@ -3,15 +3,3 @@ const rewireYarnWorkspaces = require('react-app-rewire-yarn-workspaces');
 module.exports = function override(config, env) {
   return rewireYarnWorkspaces(config, env);
 };
-
-// const fs = require("fs");
-// const path = require("path");
-// const rewireBabelLoader = require("react-app-rewire-babel-loader");
-
-// const appDirectory = fs.realpathSync(process.cwd());
-// const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
-
-// module.exports = function override(config, env) {
-//   return rewireBabelLoader.include(config, resolveApp("../shared"));
-// };
-
